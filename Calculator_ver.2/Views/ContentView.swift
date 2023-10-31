368002cea2075bbe4f272f895a590ef55ba6c81f
funMango <bluemango608@gmail.com>
2023-10-31 12:47:59 +0900

    first commit

---
+++ Calculator_ver.2/Views/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  Calculator_ver.2
+//
+//  Created by 이민호 on 2023/10/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Button("1") {
+                
+            }
+            .frame(width: 50, height: 50)
+            .foregroundColor(Color.gray)
+            
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
